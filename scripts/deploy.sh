64f275b56a0b154138718e34dd6a0b6d0ea42716
rithikpatel-git <rithik.techcanopy@gmail.com>
2026-04-16 15:54:39 +0530

    deploy.sh file changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ cd /home/ubuntu/linkedin-login
 # 1. Log into AWS ECR
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 324178491391.dkr.ecr.ap-south-1.amazonaws.com
 
-# 2. Export the variables from the file CodeBuild just handed us
-export $(cat .deploy_env | xargs)
+# 2. Export the variables from the new text file
+export $(cat deploy_env.txt | xargs)
 
 # 3. Pull the specific versions requested
 docker compose pull
