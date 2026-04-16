cd60d25ff2f70c7e35190540f82253e7956070f3
rithikpatel-git <rithik.techcanopy@gmail.com>
2026-04-16 15:07:06 +0530

    modified docker-compose file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,23 +3,17 @@
 # Navigate to the deployment directory on the EC2 server
 cd /home/ubuntu/linkedin-login
 
-# 1. Log into AWS ECR so the server can download the private images
+# 1. Log into AWS ECR
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 324178491391.dkr.ecr.ap-south-1.amazonaws.com
 
-echo "Specify client version for deployment:"
-read client_version
+# 2. Export the variables from the file CodeBuild just handed us
+export $(cat .deploy_env | xargs)
 
-echo "Specify server version for deployment:"
-read server_version
+# 3. Pull the specific versions requested
+docker compose pull
 
-# 2. Pull the newest images that CodeBuild just created
-docker pull 324178491391.dkr.ecr.ap-south-1.amazonaws.com/linkedin-client:${client_version}
-docker pull 324178491391.dkr.ecr.ap-south-1.amazonaws.com/linkedin-server:${server_version}
-
-docker tag 324178491391.dkr.ecr.ap-south-1.amazonaws.com/linkedin-client:${client_version} 324178491391.dkr.ecr.ap-south-1.amazonaws.com/mern-client:latest
-docker tag 324178491391.dkr.ecr.ap-south-1.amazonaws.com/linkedin-server:${server_version} 324178491391.dkr.ecr.ap-south-1.amazonaws.com/mern-server:latest
-# 3. Stop the old containers (if they are running)
+# 4. Stop the old containers
 docker compose down
 
-# 4. Start the new containers in the background
+# 5. Start the new containers in the background
 docker compose up -d
